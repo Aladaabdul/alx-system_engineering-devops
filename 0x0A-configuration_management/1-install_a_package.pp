3126a1407ab26f703135fbb9f69e94359d479cd3
aladarahman <aladarahman18@gmail.com>
2024-01-20 12:36:02 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,11 +8,11 @@ package { 'python3-pip':
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip',
-  require => Package['python3-pip'],
+  require  => Package['python3-pip'],
 }
 
 package { 'werkzeug':
-  ensure => '2.0.1',
+  ensure   => '2.0.1',
   provider => 'pip',
-  require => Package['python3-pip'],
+  require  => Package['python3-pip'],
 }
